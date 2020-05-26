5c9da160c05c4c1e3192fdde0afc70a192a85094
”NagatomoKazuya” <k.ngtm.0985@gmail.com>
2020-05-26 03:37:43 +0000

    PHPコース09課題提出

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+
+/*【課題3】
+「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerの
+bbbというAction に渡すRoutingの設定」を書いてみてください。*/
+Route::get('XXX', 'AAAController@bbb');
+
+
+/*【課題4】
+admin/profile/create にアクセスしたら ProfileControllerのadd Actionに、
+admin/profile/edit にアクセスしたら ProfileController の edit Action に
+割り当てるように設定してください。*/
+Route::get('admin/profile/create', 'ProfileController@add');
+Route::get('admin/profile/edit', 'ProfileController@edit');
+
+
